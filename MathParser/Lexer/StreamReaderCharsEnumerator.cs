a2d2dd410208bc182511e4e05bc62af67a6c4288
Chroniaro <zachary.carlini@gmail.com>
2019-08-21 00:44:22 -0400

    Switched to using native iterator for character stream

---
+++ MathParser/Lexer/StreamReaderCharsEnumerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+[assembly: InternalsVisibleTo("Tests")]
+namespace MathParser.Lexer
+{
+    internal static class StreamReaderCharsEnumerator
+    {
+        public static IEnumerable<char> Chars(this StreamReader reader)
+        {
+            while (true)
+            {
+                int next = reader.Read();
+                if (next == -1)
+                    yield break;
+                else
+                    yield return (char)next;
+            }
+        }
+    }
+}
---
+++ MathParserTests/Lexer/StreamReaderCharsEnumeratorTest.cs
@@ -0,0 +1,34 @@
+﻿using MathParser;
+using MathParser.Lexer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MathParserTests.Lexer
+{
+    [TestClass]
+    public class StreamReaderCharsEnumeratorTest
+    {
+        [TestMethod]
+        [DataRow("")]
+        [DataRow("foo")]
+        [DataRow("f\no§b")]
+        public void Chars_StreamFromUTF8StringBytes_EqualToCharactersOfString(string data)
+        {
+            //set up
+            var encoding = Encoding.UTF8;
+            var bytes = encoding.GetBytes(data);
+            var stream = new MemoryStream(bytes);
+            var reader = new StreamReader(stream, encoding);
+
+            //act
+            var chars = reader.Chars();
+
+            //test
+            CustomAssertions.AssertEqual(data, chars);
+        }
+    }
+}
