b850beb0b5806a3eeb02e23c0d8a0bf08bf95bb4
Chroniaro <zachary.carlini@gmail.com>
2019-09-18 00:50:35 -0400

    Added Trie.rm dependency to improve delimiter lexing

--- MathParser/Lexer/DelimiterLexer.cs
+++ MathParser/Lexer/DelimiterLexer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rm.Trie;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,70 +7,59 @@ namespace MathParser.Lexer
 {
     class DelimiterLexer : AbstractLexer
     {
+        public delegate DelimiterToken TokenConstructor(string content);
+
+        private static readonly TokenConstructor DEFAULT_CONSTRUCTOR = content => new DelimiterToken(content);
+
         private static readonly string[] DEFAULT_DELIMITERS =
         {
             "+", "-", "*", "/"
         };
 
-        public SortedSet<string> Delimiters { get; }
+        public TrieMap<TokenConstructor> Delimiters { get; }
 
         public DelimiterLexer()
         {
-            Delimiters = new SortedSet<string>();
+            Delimiters = new TrieMap<TokenConstructor>();
         }
 
-        public DelimiterLexer UseDelimiters(params string[] delimiters)
+        public DelimiterLexer UseDelimiters(TokenConstructor constructor, params string[] delimiters)
         {
             foreach (string delimiter in delimiters)
-                Delimiters.Add(delimiter);
+                Delimiters.Add(delimiter, constructor);
 
             return this;
         }
+
+        public DelimiterLexer UseDelimiters(params string[] delimiters) =>
+            UseDelimiters(DEFAULT_CONSTRUCTOR, delimiters);
 
         public DelimiterLexer UseDefaultDelimiters => UseDelimiters(DEFAULT_DELIMITERS);
 
         public override Token? Lex(TokenBuilder tokenBuilder)
         {
-            var delimiters = Delimiters.GetEnumerator();
-            string? lastMatch = null;
+            (TokenConstructor, string)? lastFullDelimiter = null;
+
             var delimiterBuilder = new StringBuilder(5);
-            while (FindNextMatch(tokenBuilder, delimiterBuilder, delimiters) is string match)
-                lastMatch = match;
+            var trieNode = Delimiters.GetRootTrieNode();
 
-            if (lastMatch == null)
-                return null;
+            while (tokenBuilder.MoveNext())
+            {
+                char c = tokenBuilder.Current;
+                if (!trieNode.HasChild(c))
+                    break;
+
+                trieNode = trieNode.GetChild(c);
+                delimiterBuilder.Append(c);
+
+                if (trieNode.HasValue())
+                    lastFullDelimiter = (trieNode.Value, delimiterBuilder.ToString());
+            }
+
+            if (lastFullDelimiter is (TokenConstructor constructor, string delimiter))
+                return constructor(delimiter);
             else
-                return new DelimiterToken(lastMatch);
-        }
-
-        private string? FindNextMatch(TokenBuilder tokenBuilder, StringBuilder matchBuilder, IEnumerator<string> delimiters)
-        {
-            try
-            {
-                tokenBuilder.MoveNext();
-                matchBuilder.Append(tokenBuilder.Current);
-
-                string match = matchBuilder.ToString();
-                do
-                    delimiters.MoveNext();
-                while (string.Compare(delimiters.Current, match) < 0);
-
-                while (string.Compare(delimiters.Current, match) > 0)
-                {
-                    tokenBuilder.MoveNext();
-                    matchBuilder.Append(tokenBuilder.Current);
-                    match = matchBuilder.ToString();
-                }
-
-                if (match == delimiters.Current)
-                    return match;
-                else
-                    return null;
-            }
-            catch (ArgumentOutOfRangeException)
-            {
                 return null;
-            }
         }
     }
 }
